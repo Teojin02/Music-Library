866246950c6244fd11ff1ebf2e2e19b334df17fd
Your Name <you@example.com>
2020-10-09 09:09:49 -0700

    updated app.js

--- basketball/src/App.js
+++ basketball/src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import axios from "axios"
+import axios from "axios";
 
 class App extends Component {
-  constructor (props) {
+  constructor(props){
     super(props)
     this.state={
       playerName: null,
@@ -12,83 +12,73 @@ class App extends Component {
 
 handleSubmit = (e) => {
   e.preventDefault();
-  this.getPlayerId()  
+  this.getPlayerId()
   console.log(this.state.playerName)
 }
 
-handlechange = (event) => {
-const replace = event.target.value.split(" ").join("_");
-if(replace.length > 0){
-  this.setState({playerName: replace})
-}else {
-  alert("Please type players name!")
+handleChange = (event) => {
+  const replace = event.target.value.split(" ").join("_");
+  if(replace.length > 0){
+    this.setState({playerName: replace})
+
+  }
 }
 
-}
+  getPlayerId = () => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
+    .then(async res => {
+      // console.log(res.data.data)
+      if(res.data.data[0] === undefined){
+        alert("This player is either injured or hasn't played yet!")
+      } else if(res.data.data.length > 1){
+        alert("Pleases specify the name more!")
+      } else{
+        await this.getPlayerStats(res.data.data[0].id)
 
-
-
-
-getPlayerId = () => {
-axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`) 
-.then(async res => {
-  //console.log(res.data.data)
-  if(res.data.data[0] === undefined){
-     alert("This player is either injured or hasn't played yet!")
-} else if (res.data.data.length > 1){
-    alert("Please specify the name more!")
-  } else{
- await this.getPlayerStats(res.data.data[0].id)
+      }
+    }).catch(err => {
+      console.log(err)
+    })
   }
 
-}).catch(err => {
-  console.log(err)
-})
-
-}
-
-
-getPlayerStats = (playerId) => {
-  axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2019&player_ids[]=${playerId}`)
-  .then(async res =>{
-    console.log(res.data.data)
-    this.setState({playerStats: res.data.data[0]})
-  }).catch(err => {
-    console.log(err)
-  })
-}
-
-componentDidMount(){
-  this.getPlayerId()
-  this.getPlayerStats()
-}
-
-  render () {
-      return (
+  getPlayerStats = (playerId) => {
+    axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=1997&player_ids[]=${playerId}`)
+    .then(async res => {
+      console.log(res.data.data)
+      this.setState({ playerStats: res.data.data[0]})
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+  
+  render(){
+  return (
     <div className="App">
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
+     <form onSubmit={this.handleSubmit}>
+       <label>
+         Name
+         <input 
           type="text"
           value={this.state.value}
           onChange={this.handleChange}
-          placeholder= "please enter players name"
-          />
-        </label>
-        <input type="submit" value="submit"/>
-      </form>
-      games played: {this.state.playerStats["games_played"]}
+          placeholder="please enter players name"
+         />
+       </label>
+       <input type="submit" value="Submit"/>
+     </form>
+     games played: {this.state.playerStats["games_played"]}
      <br />
      points averaged: {this.state.playerStats["pts"]}
      <br />
      rebounds averaged: {this.state.playerStats["reb"]}
      <br />
      assists averaged: {this.state.playerStats["ast"]}
+     <br />
+     steals averaged : {this.state.playerStats["stl"]}
+     <br />
+     Minutes average: {this.state.playerStats["min"]}
     </div>
   );
 }
-  }
-
-
+}
 export default App;
