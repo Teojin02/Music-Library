dd481be6e2bd49b7234848ac9b6b9625e1982ada
Your Name <you@example.com>
2020-10-14 02:39:43 -0700

    updated search.js

--- src/pages/components/Search.js
+++ src/pages/components/Search.js
@@ -1,12 +1,28 @@
 
 import React, { Component } from "react";
 import "./style/Search.css"
+
+let FakeServerData = {
+user: {
+  name: "Teojin",
+  playlists: [
+    {
+      name:"My favorites",
+      songs: ["Beat it", "Bad", "smooth criminal"]
+    },
+    { name:"My food",
+      songs: ["ding dong", "water", "table"]},
+    { name:"My nae",
+      songs: ["trick", "snoot", "fan"]},
+  ]
+}
+}
 class Songs extends Component {
 render () {
   return (
     <div>
       <h1>
-        Artist
+        {this.props.playlists && this.props.playlists.length}
       </h1>
       <ul>
         <li>Song 1</li>
@@ -29,13 +45,31 @@ class Find extends Component {
 
 
 class Search extends Component {
+  constructor() {
+    super ();
+    this.state = {ServerData: {}}
+  }
+  componentDidMount() {
+    setTimeout (() => {
+      this.setState({ServerData: FakeServerData});
+    }, 1000);
+  }
   render() {
     return (
       <div className="Search">
+        {this.state.ServerData.user ?
+        <div>
+        <h1>
+        {this.state.ServerData.user.name}'s Playlist
+        </h1>
+
        <Find/>
+       <Songs playlists={this.state.ServerData.user && 
+                        this.state.ServerData.playlists}/>
        <Songs/>
        <Songs/>
-       <Songs/>
+       </div> : <h1>Loading...</h1>
+  } 
       </div>
     );
   }
