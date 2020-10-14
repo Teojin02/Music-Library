26f1fb0b8ecdfbea7fc37ad06cc82150866cd6fc
Your Name <you@example.com>
2020-10-14 01:41:46 -0700

    updated Search.js

--- src/pages/components/Search.js
+++ src/pages/components/Search.js
@@ -1,18 +1,45 @@
-import React from "react";
-import {Form, FormControl, Button} from 'react-bootstrap';
 
-
-
-function Search() {
+import React, { Component } from "react";
+import "./style/Search.css"
+class Songs extends Component {
+render () {
   return (
-<div>
-     <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-primary">Search</Button>
-    </Form>
-</div>
-
-  );
+    <div>
+      <h1>
+        Artist
+      </h1>
+      <ul>
+        <li>Song 1</li>
+        <li>Song 2</li>
+        <li>Song 3</li>
+         
+      </ul>
+    </div>
+  )
 }
+}
+
+class Find extends Component {
+  render() {
+    return (
+       <input type="text" placeholder="Search.."/>
+    )
+  }
+}
+
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="Search">
+       <Find/>
+       <Songs/>
+       <Songs/>
+       <Songs/>
+      </div>
+    );
+  }
+}
+
 
 export default Search;
