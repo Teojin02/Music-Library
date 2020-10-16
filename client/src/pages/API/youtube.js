fe269ac5e5ed4de318e2f31426a3571158e496c5
Your Name <you@example.com>
2020-10-15 17:51:07 -0700

    update server.js

---
+++ client/src/pages/API/youtube.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const KEY = 'AIzaSyA-mMGH7oJGDVfveEAKLmdLsufIeRohr0Y';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+})
